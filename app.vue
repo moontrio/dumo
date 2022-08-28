08cd7e9d4c14ba8dd4191e49920f325ee55bd584
youngcong <1151651564@qq.com>
2022-08-28 22:49:04 +0800

    feat: favicon

--- app.vue
+++ app.vue
@@ -5,6 +5,9 @@ useHead({
     { name: 'description', content: 'Dumo Music' },
     { name: 'keywords', content: 'Nuxt 3' },
   ],
+  link: [
+    { rel: 'icon', type: 'image/png', href: '/icon.png' },
+  ],
 })
 </script>
 
